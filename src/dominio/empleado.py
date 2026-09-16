b3209a505def4b6e5552b7a6a14b531875008971
Maximiliano <maxicardozo.paredes4431@gmail.com>
2026-09-16 11:42:13 -0300

    Definicion de la clase empleado

---
+++ src/dominio/empleado.py
@@ -0,0 +1,17 @@
+
+class Empleado:
+    """Clase que representa a un empleado"""
+
+    def __init__(self, nombre:str, correo:str, telefono:str, id:int, sueldo:int):
+        self.nombre = nombre
+        self.correo = correo
+        self.telefono = telefono
+        self.id = id
+        self.sueldo = sueldo
+
+
+    def mostrardatos(self) -> str:
+        return f"Nombre: {self.nombre}, Correo: {self.correo}, Teléfono: {self.telefono}, ID: {self.id}"
+
+    def calcular_pago(self) -> int:
+        return self.sueldo
